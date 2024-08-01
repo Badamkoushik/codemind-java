6e44246b63ac6d33f15c9d24501c4f0f3fd793c8
Badamkoushik <147793162+Badamkoushik@users.noreply.github.com>
2024-08-01 11:38:34 +0530

    import java.util.Scanner; public class Example{     public static void main(String[] args)     {         int a,b;         Scanner read=new Scanner(System.in);         a=read.nextInt();         b=read.nextInt();         int res;         res=a*b;         System.out.println(res);     } }

---
+++ Instant_Noodles.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Example{
+    public static void main(String[] args)
+    {
+        int a,b;
+        Scanner read=new Scanner(System.in);
+        a=read.nextInt();
+        b=read.nextInt();
+        int res;
+        res=a*b;
+        System.out.println(res);
+    }
+}
